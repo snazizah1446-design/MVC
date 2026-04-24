cc31a2de71218631a52d7e699caf040fa29ffd4c
Siti Nur Azizah <snazizah1446@gmail.com>
2026-04-24 14:38:19 +0100

    MahasiswaController

---
+++ Controller/MahasiswaController.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "model/Mahasiswa.php"; 
+
+class MahasiswaController {
+
+    public function index() {
+        $model = new Mahasiswa();      
+        $data = $model->getData();     
+
+        require "view/mahasiswa_view.php"; 
+    }
+}
